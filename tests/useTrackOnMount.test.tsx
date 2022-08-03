9956c860ba364267ea5e1a3ce636eca896f06320
Evgeniy Valyaev <e.valiaev@indriver.com>
2022-08-04 00:42:40 +0300

    test: added two cases

--- tests/useTrackOnMount.test.tsx
+++ tests/useTrackOnMount.test.tsx
@@ -7,6 +7,8 @@ jest.mock('@amplitude/analytics-browser', () => ({
 	track: jest.fn(),
 }));
 
+beforeEach(() => jest.clearAllMocks());
+
 describe('useTrackOnMount', () => {
 	it('track should call', () => {
 		function Component() {
@@ -16,6 +18,28 @@ describe('useTrackOnMount', () => {
 		render(<Component />);
 
 		expect(track).toBeCalledTimes(1);
-		expect(track).toBeCalledWith('test');
+		expect(track).toBeCalledWith('test', undefined, undefined);
+	});
+
+	it('track should call with event property', () => {
+		function Component() {
+			useTrackOnMount('test', { test_prop: 'test_event_property' });
+			return <div></div>;
+		}
+		render(<Component />);
+
+		expect(track).toBeCalledTimes(1);
+		expect(track).toBeCalledWith('test', { test_prop: 'test_event_property' }, undefined);
+	});
+
+	it('track should call with event property and event option', () => {
+		function Component() {
+			useTrackOnMount('test', { test_prop: 'test_event_property' }, { android_id: 'test_android_id' });
+			return <div></div>;
+		}
+		render(<Component />);
+
+		expect(track).toBeCalledTimes(1);
+		expect(track).toBeCalledWith('test', { test_prop: 'test_event_property' }, { android_id: 'test_android_id' });
 	});
 });
