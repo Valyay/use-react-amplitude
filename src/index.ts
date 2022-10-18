dc0760924e49bd1acb6687d5d0ed37b33720165a
Evgeniy Valyaev <e.valiaev@indriver.com>
2022-10-18 22:23:42 +0500

    fix: change path useTrackOnUpdate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { useTrackOnMount } from './useTrackOnMount';
 import { useTrackOnChange } from './useTrackOnChange';
-import { useTrackOnUpdate } from 'useTrackOnUpdate';
+import { useTrackOnUpdate } from './useTrackOnUpdate';
 
 export { useTrackOnMount, useTrackOnChange, useTrackOnUpdate };
